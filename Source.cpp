e503bf3c1dfcc6bb7e14719b1382d098abf56124
Nicolas <ariaslopez120010@gmail.com>
2024-12-30 14:40:18 -0500

    Initializing SDL and creating the window and the renderer.

---
+++ Source.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<SDL.h>
+
+SDL_Window* g_pWindow = 0;
+SDL_Renderer* g_pRenderer = 0;
+
+int main(int argc, char* args[]) {
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		std::cout << "Error initializing SDL: " << SDL_GetError;
+		return -1;
+	}
+
+	g_pWindow = SDL_CreateWindow("Setting up SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+
+	if (!g_pWindow) {
+		std::cout << "Error creating the window: " << SDL_GetError;
+		return -1;
+	}
+	
+	g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+
+	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+
+	SDL_RenderClear(g_pRenderer);
+
+	SDL_RenderPresent(g_pRenderer);
+
+	SDL_Delay(5000);
+
+	SDL_Quit();
+
+	std::cout << "Error creating the: ";
+	return 0;
+}
