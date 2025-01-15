eeeccdfd395971aa9eb6a51f644464b58ce9133c
Nicolas <ariaslopez120010@gmail.com>
2025-01-14 22:54:45 -0500

    Creating kind of a terrain.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char* args[]) {
 
-	if (!TheGame::Instance()->init("Chapter 1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, NULL))
+	if (!TheGame::Instance()->init("Chapter 1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_RESIZABLE))
 		return -1;
 
 	while (TheGame::Instance()->running()) {
